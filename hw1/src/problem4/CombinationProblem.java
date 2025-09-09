771855813d0b3989c172983ea8279fe734ba2856
jay <immaspring2024@gmail.com>
2025-09-09 16:59:05 -0400

    solved problem 4

--- hw1/src/problem4/CombinationProblem.java
+++ hw1/src/problem4/CombinationProblem.java
@@ -1,7 +1,26 @@
 package problem4;
 
+import java.util.Scanner;
+
 public class CombinationProblem {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Please input a positive integer: ");
+    int value = scanner.nextInt();
 
+    while (value < 0) {
+      System.out.println("Input must be greater than 0:");
+      value = scanner.nextInt();
+    }
+
+    int count = 0;
+    for (int i = 1; i < value; i++) {
+      for (int j = i + 1; j <= value; j++) {
+        System.out.printf("%d %d\n", i, j);
+        count++;
+      }
+    }
+
+    System.out.printf("Total number of combinations is: %d\n", count);
   }
 }
